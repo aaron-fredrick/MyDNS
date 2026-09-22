f7d93f9209b9ab7a81c6cb2e5cf319ee48554dd2
Aaron Fredrick <Aaron_Fredrick@proton.me>
2026-09-22 12:58:06 +0530

    test: simplify content type assertion in response tests

--- src/mydns/error/mod.rs
+++ src/mydns/error/mod.rs
@@ -50,14 +50,12 @@ mod tests {
         let response = error.into_response();
 
         assert_eq!(response.status(), status);
-        assert!(
-            response
-                .headers()
-                .get(axum::http::header::CONTENT_TYPE)
-                .and_then(|value| value.to_str().ok())
-                .unwrap()
-                .starts_with("application/json")
-        );
+        assert!(response
+            .headers()
+            .get(axum::http::header::CONTENT_TYPE)
+            .and_then(|value| value.to_str().ok())
+            .unwrap()
+            .starts_with("application/json"));
 
         let body = to_bytes(response.into_body(), usize::MAX).await.unwrap();
         let json: Value = serde_json::from_slice(&body).unwrap();
