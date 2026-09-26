fdc5ca88dad07206854b50f184f67f5192f9ca48
Aaron Fredrick <Aaron_Fredrick@proton.me>
2026-09-26 15:25:43 +0530

    Expose top-level metrics aggregator

--- src/mydns/observability/telemetry/metrics/mod.rs
+++ src/mydns/observability/telemetry/metrics/mod.rs
@@ -1,6 +1,19 @@
 pub mod aggregation;
+pub mod aggregator;
 pub mod domains;
 pub mod persistence;
 pub mod types;
+
+pub use aggregator::{
+    ApiOperationalMetrics,
+    ApiPerformanceMetrics,
+    DatabaseOperationalMetrics,
+    DatabasePerformanceMetrics,
+    DnsOperationalMetrics,
+    DnsPerformanceMetrics,
+    MetricsAggregator,
+    OperationalMetrics,
+    PerformanceMetrics,
+};
 
 pub use domains::dns::DnsAggregator;
