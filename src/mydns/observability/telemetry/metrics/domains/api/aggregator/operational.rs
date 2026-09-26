c17a9bcb3d89d6f7ec40c14ae706e1ea4ac9ff4a
Aaron Fredrick <Aaron_Fredrick@proton.me>
2026-09-26 18:33:24 +0530

    feat: tally API request and response bytes

--- src/mydns/observability/telemetry/metrics/domains/api/aggregator/operational.rs
+++ src/mydns/observability/telemetry/metrics/domains/api/aggregator/operational.rs
@@ -6,6 +6,9 @@ pub struct ApiOperationalAggregator {
     pub requests: ScalarCounter,
     pub responses: ScalarCounter,
 
+    pub request_size_bytes: ScalarCounter,
+    pub response_size_bytes: ScalarCounter,
+
     pub authentication_successes: ScalarCounter,
     pub authentication_failures: ScalarCounter,
 
